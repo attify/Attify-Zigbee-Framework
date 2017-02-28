90dc484b8105e0e291d1cbe72a1fde5b2408c7b3
txjoe <tabish.imran96@gmail.com>
2017-02-28 15:56:10 +0530

    Created file for installer.sh

---
+++ installer.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+echo
+echo "      ___  _   _   _  __            "
+echo "     / _ \| | | | (_)/ _|           "
+echo "    / /_\ \ |_| |_ _| |_ _   _      "
+echo "    |  _  | __| __| |  _| | | |     "
+echo "    | | | | |_| |_| | | | |_| |     "
+echo "    \_| |_/\__|\__|_|_|  \__, |     "
+echo "                          __/ |     "
+echo "                         |___/      "
+echo
+echo "[*] Attify Zigbee Framework v1.0 Installer"
+
+
+sleep 1
+
+echo "[*] Installer under construction, lulz "
+echo
+echo "[*] Bye now!"
+
