a1fed5463a127325a6e7af1fa15225e04c304b24
txjoe <tabish.imran96@gmail.com>
2017-03-03 14:46:14 +0530

    Mockup

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-#!/usr/bin/python
+from PyQt4 import QtCore,QtGui
+from UI.Main import Ui_MainWindow
+from UI.Config import Ui_Form
+import sys
 
-print "[*] Zigbee exploitation tool "
+class ZBMain(Ui_MainWindow):
+	def __init__(self,dialog,parent=None):
+		Ui_MainWindow.__init__(self)
+		self.setupUi(dialog)
 
+if __name__=="__main__":
+	app=QtGui.QApplication(sys.argv)
+	dialog=QtGui.QMainWindow()
+        app.setWindowIcon(QtGui.QIcon("UI/icon.png"))
+	prog=ZBMain(dialog)
+	dialog.show()
+	sys.exit(app.exec_())
