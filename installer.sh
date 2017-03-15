14d600ea0d917ec6e803689c7b60557cf4614752
txjoe <tabish.imran96@gmail.com>
2017-03-15 16:55:27 +0530

    Fixed installer

--- installer.sh
+++ installer.sh
@@ -1,4 +1,10 @@
 !/bin/bash
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+
+printf "${RED}"
 echo
 echo "      ___  _   _   _  __            "
 echo "     / _ \| | | | (_)/ _|           "
@@ -10,11 +16,28 @@ echo "                          __/ |     "
 echo "                         |___/      "
 echo
 echo "[*] Attify Zigbee Framework v1.0 Installer"
-
+printf "${NORMAL}"
 
 sleep 1
 
-echo "[*] Installer under construction, lulz "
-echo
+printf "${GREEN}[*] Installing dependencies ${NORMAL}\n\n"
+sudo apt-get install python-gtk2 python-cairo python-usb python-crypto python-serial python-dev libgcrypt-dev
+
+printf "${GREEN}[*] Installing Killerbee ${NORMAL}\n\n"
+cd killerbee
+sudo python setup.py install
+
+printf "${GREEN}[*] All done! ${NORMAL} "
+
 echo "[*] Bye now!"
 
+printf "${RED}"
+echo " _______________"
+echo "( Happy hacking )"
+echo " ---------------"
+echo "        o   ^__^"
+echo "         o  (oo)\_______"
+echo "            (__)\       )\/\"
+echo "                ||----w |"
+echo "                ||     ||"
+printf "${NORMAL}\n"
